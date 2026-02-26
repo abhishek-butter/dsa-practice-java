58f6e59d22564772cec947abc4a8d95e2c5770a6
Abhishek <abhishekvaiybhav@gmail.com>
2026-02-26 22:11:43 +0530

    Time: 2 ms (99.39%) | Memory: 44.6 MB (97.70%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int a[]=new int[nums1.length];
+        Stack<Integer> s=new Stack<>();
+        for(int j=0;j<nums1.length;j++){
+
+            a[j]=finder(nums1[j],nums2);
+        }
+        return a;
+        
+
+
+        
+    }
+    public int finder(int a,int[] b){
+        for(int i=0;i<b.length;i++){
+            if(b[i]==a){
+                
+                
+                while(i<b.length){
+                    
+                    if(b[i]>a){
+                        return b[i];
+
+                    }
+                    i++;
+
+                }
+
+            }
+        }
+        return -1;
+    }
+}
