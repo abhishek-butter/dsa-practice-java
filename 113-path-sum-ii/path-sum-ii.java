d454eda56a81646fc93df729c10da7c23dce39e9
Abhishek <abhishekvaiybhav@gmail.com>
2026-04-11 23:12:14 +0530

    Time: 1 ms (100.00%) | Memory: 45.2 MB (94.84%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.java
+++ 113-path-sum-ii/path-sum-ii.java
@@ -17,22 +17,28 @@ class Solution {
     List<List<Integer>> l;
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
         l=new ArrayList<>();
-        DFS(root,targetSum,new ArrayList<>());
+        if(root==null)return l;
+        path(root,targetSum,new ArrayList<>());
         return l;
+        
     }
-    public void DFS(TreeNode root,int t,List<Integer> pocket){
-        if(root==null)return;
+    public void path(TreeNode root,int t,List<Integer> pocket){
+        
+        
+
         pocket.add(root.val);
-        if(root.right==null && root.left==null && t==root.val){
+        if(root.left==null&&root.right==null&&root.val==t){
             l.add(new ArrayList<>(pocket));
-
-        }
-        else{
-            if(root.left!=null)DFS(root.left,t-root.val,pocket);
-            if(root.right!=null)DFS(root.right,t-root.val,pocket);
-
+            
+            
         }
 
+        if(root.left!=null)path(root.left,t-root.val,pocket);
+        
+        if(root.right!=null)path(root.right,t-root.val,pocket);
         pocket.remove(pocket.size()-1);
+        
+
+        
     }
 }
