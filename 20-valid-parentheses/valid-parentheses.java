93eb701ae58e6d70422b6113150bca1575930689
Abhishek <abhishekvaiybhav@gmail.com>
2026-04-23 20:31:26 +0530

    Time: 2 ms (97.97%) | Memory: 43.4 MB (28.04%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,23 +1,23 @@
 class Solution {
     public boolean isValid(String s) {
         Deque<Character> d=new ArrayDeque<>();
-        if(s.length()==0)return true;
-        
+
+
 
         for(char c:s.toCharArray()){
 
-            if(c=='('||c=='['||c=='{')d.push(c);
+            if(c=='['||c=='{'||c=='('){
+                d.push(c);
+            }
             else{
                 if(d.isEmpty())return false;
                 char top=d.pop();
 
-                
                 if(c==')' && top!='(')return false;
                 if(c==']' && top!='[')return false;
                 if(c=='}' && top!='{')return false;
-
             }
-
+            
         }
         return d.isEmpty();
         
