78b6945f7fd7b3faa5146e0ba14dcd05fee2acb3
Abhishek <abhishekvaiybhav@gmail.com>
2026-05-06 23:03:07 +0530

    Time: 3 ms (33.80%) | Memory: 45.8 MB (50.42%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,22 +1,24 @@
 class Solution {
     List<List<Integer>> l;
-
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         l=new ArrayList<>();
-        recursion(0,candidates,target,new ArrayList<>());
-        return l;   
+        rec(0,candidates,target,new ArrayList<>());
+
+        return l;
+        
     }
-    public void recursion(int i,int[] c,int sum,List<Integer> pocket){
-        if(sum==0){
-            l.add(new ArrayList(pocket));
-            return;   
-        }
-        if (i == c.length || sum < 0) {
+    public void rec(int a,int[] c,int s,List<Integer> pocket){
+        if(s==0){
+            l.add(new ArrayList<>(pocket));
             return;
         }
-        pocket.add(c[i]);
-        recursion(i,c,sum-c[i],pocket);
-        pocket.remove(pocket.size()-1);
-        recursion(i+1,c,sum,pocket);        
+        if(s<0){
+            return;
+        }
+        for(int i=a;i<c.length;i++){
+            pocket.add(c[i]);
+            rec(i,c,s-c[i],pocket);
+            pocket.remove(pocket.size()-1);
+        }
     }
 }
