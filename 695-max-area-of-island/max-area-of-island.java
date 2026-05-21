d44acce8dbf37cced709b9d056de71f607c1f823
Abhishek <abhishekvaiybhav@gmail.com>
2026-05-21 23:19:58 +0530

    Time: 1 ms (100.00%) | Memory: 46.5 MB (66.86%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,29 +1,28 @@
 class Solution {
-    public int maxAreaOfIsland(int[][] grid) {
+    
 
-        int Maxcount=0;
+    public int maxAreaOfIsland(int[][] grid) {
+        int max=0;
+
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
-                int count=dfs(grid,i,j);
-                Maxcount=Math.max(count,Maxcount);
+                if(grid[i][j]==1){
+                    max=Math.max(max,dfs(grid,i,j));
+                }
             }
         }
-        return Maxcount;
+        return max;
         
     }
-
     public int dfs(int[][] g,int i,int j){
-        if(i<0||i>g.length-1||j<0||j>g[0].length-1||g[i][j]==0)return 0;
+        if(i<0||j<0||i>g.length-1||j>g[0].length-1||g[i][j]==0)return 0;
 
         g[i][j]=0;
+        int up=dfs(g,i-1,j);
+        int down=dfs(g,i+1,j);
+        int right=dfs(g,i,j+1);
+        int left=dfs(g,i,j-1);
 
-        int right=dfs(g,i+1,j);
-        int down=dfs(g,i,j+1);
-        int left= dfs(g,i-1,j);
-        int up=dfs(g,i,j-1);
-        return 1+(up+down+left+right);
-
-
-
+        return (1+up+down+right+left);
     }
 }
