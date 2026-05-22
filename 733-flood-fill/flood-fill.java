48e1eb1b3a96559589d26c321c3d97c39d4f276f
Abhishek <abhishekvaiybhav@gmail.com>
2026-05-22 21:35:06 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (24.19%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,36 +1,15 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        Queue<int[]> q=new LinkedList<>();
-        q.add(new int[]{sr,sc});
-        int prevcolour=image[sr][sc];
-        int visited[][]=new int[image.length][image[0].length];
-        while(!q.isEmpty()){
-            int arr[]=q.poll();
-            int r=arr[0];
-            int c=arr[1];
-            visited[r][c]=1;
-            
-            image[r][c]=color;
-            
-            if(r+1<image.length && image[r+1][c]==prevcolour && visited[r+1][c]==0){
-                q.add(new int[]{r+1,c});
-            }
-            if(c+1<image[0].length && image[r][c+1]==prevcolour && visited[r][c+1]==0){
-                q.add(new int[]{r,c+1});
-            }
-            if(r-1>-1 && image[r-1][c]==prevcolour && visited[r-1][c]==0){
-                q.add(new int[]{r-1,c});
-            }
-            if(c-1>-1 && image[r][c-1]==prevcolour && visited[r][c-1]==0){
-                q.add(new int[]{r,c-1});
-            }
-
-
-            
-            
-        }
-        return image;
-
-        
+        if(image[sr][sc]==color)return image;
+        dfs(image,sr,sc,image[sr][sc],color);
+        return image;        
+    }
+    public void dfs(int[][] image,int i,int j,int s,int color){
+        if(i<0||j<0||i>image.length-1||j>image[0].length-1||image[i][j]!=s)return;
+        image[i][j]=color;
+        dfs(image,i+1,j,s,color);
+        dfs(image,i-1,j,s,color);
+        dfs(image,i,j+1,s,color);
+        dfs(image,i,j-1,s,color);
     }
 }
