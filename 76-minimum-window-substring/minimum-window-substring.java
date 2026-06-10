8f3f8b9d8aaea276604df47aee960ee53c3545ee
Abhishek <abhishekvaiybhav@gmail.com>
2026-06-10 08:17:26 +0530

    Time: 115 ms (12.52%) | Memory: 48.3 MB (5.35%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,34 +1,43 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int present[]=new int[128];
-        for(char c:t.toCharArray()){
-            present[c]++;
-        }
-        int req=t.length();
-        int left=0;
-        int v=99999999;
+        int val=99999999;
         String ans="";
-        
-        for(int i=0;i<s.length();i++){
-            present[s.charAt(i)]--;
-            if(present[s.charAt(i)]>-1){
-                req--;
-            }
-            
-            
-            while(req==0){
-                if(i-left+1<v){
-                    ans=s.substring(left,i+1);
-                    v=i-left+1;
-                }             
-                present[s.charAt(left)]++;
-                if(present[s.charAt(left)]>0)req++;
-                left++;
-
-            }
-            
-
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(char c:t.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
         }
+        int left=0;
+
+        int req=t.length();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            map.put(c,map.getOrDefault(c,0)-1);
+            if(map.get(c)>=0){
+                
+                req--;
+                
+            }
+            while(req==0){
+                if(i-left+1<val){
+                    val=i-left+1;
+                    ans=s.substring(left,i+1);
+                    
+                    
+                }
+                
+            if(map.containsKey(s.charAt(left))){
+                map.put(s.charAt(left),map.get(s.charAt(left))+1);
+                if(map.get(s.charAt(left))>0)req++;
+                
+
+            }
+            left++;
+            
+            
+
+            }
+        }
+
         return ans;
         
     }
