62afd9bff63c635e8eae8407234491f325a6ae67
Abhishek <abhishekvaiybhav@gmail.com>
2026-06-14 21:00:46 +0530

    Time: 84 ms (54.45%) | Memory: 103.1 MB (25.80%) - LeetSync

--- 883-car-fleet/car-fleet.java
+++ 883-car-fleet/car-fleet.java
@@ -1,10 +1,12 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
+        if(position.length<1)return 0;
         List<double[]> l=new ArrayList<>();
         for(int i=0;i<position.length;i++){
             l.add(new double[]{(double)(position[i]),(double)(target-position[i])/speed[i]});
         }
         l.sort((a,b)->Double.compare(b[0],a[0]));
+        
         double prev=l.get(0)[1];
         int fleet=1;
         
