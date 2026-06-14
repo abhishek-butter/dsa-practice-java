178dcf58c74fcf079976abea1ad6c8ae8d3dcf25
Abhishek <abhishekvaiybhav@gmail.com>
2026-06-14 21:26:50 +0530

    Time: 2 ms (71.67%) | Memory: 44.9 MB (22.62%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,31 @@
+class Solution {
+    List<String> l;
+    public List<String> generateParenthesis(int n) {
+        l=new ArrayList<>();
+        backtrack(n,0,0,"");
+        return l; 
+    }
+    public void backtrack(int n,int open,int close,String s){
+        if(close>open)return;
+        if(s.length()==2*n){
+            
+            l.add(s);
+            return;
+        }
+        if(open<n){
+            
+            backtrack(n,open+1,close,s+"(");
+            
+        }
+       
+        
+        if(close<open){
+            
+            backtrack(n,open,close+1,s+")");
+            
+
+        }
+        
+        return;
+    }
+}
