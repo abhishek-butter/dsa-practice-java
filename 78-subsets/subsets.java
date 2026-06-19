996cbbad1ab1af1cf29843b93f315d5ba052c8f0
Abhishek <abhishekvaiybhav@gmail.com>
2026-06-19 22:14:16 +0530

    Time: 1 ms (88.77%) | Memory: 44.3 MB (20.44%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,24 +1,20 @@
 class Solution {
     List<List<Integer>> l;
-
     public List<List<Integer>> subsets(int[] nums) {
         l=new ArrayList<>();
-        rec(0,nums,new ArrayList<>());
+        rec(nums,0,new ArrayList<>());
         return l;
-
-
+    
         
     }
-
-    public void rec(int index,int n[],List<Integer> pocket){
-        if(index==n.length){
+    public void rec(int[] nums,int i,List<Integer> pocket){
+        if(i==nums.length){
             l.add(new ArrayList<>(pocket));
             return;
         }
-        pocket.add(n[index]);
-        rec(index+1,n,pocket);
+        pocket.add(nums[i]);
+        rec(nums,i+1,pocket);
         pocket.remove(pocket.size()-1);
-        rec(index+1,n,pocket);
-        return;
+        rec(nums,i+1,pocket);
     }
 }
