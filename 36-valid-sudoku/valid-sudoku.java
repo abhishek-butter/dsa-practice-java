7781b44db974f9ba359c330d4be93eed31d71351
Abhishek <abhishekvaiybhav@gmail.com>
2026-07-23 19:00:57 +0530

    Time: 2 ms (68.59%) | Memory: 46.6 MB (45.93%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,70 +1,56 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
+        HashSet<Character> s1=new HashSet<>();
+        HashSet<Character> s2=new HashSet<>();
+        HashSet<Character> s3=new HashSet<>();
 
-        HashMap<Character,Integer> map1=new HashMap<>();
-        for(int i=0;i<board.length;i++){
-            map1=new HashMap<>();
-            for(int j=0;j<board[0].length;j++){
-                char a=board[i][j];
-                if(a=='.')continue;
-                map1.put(a,map1.getOrDefault(a,0)+1);
-                if(map1.get(a)>1)return false;
+        int imax=board.length;
+        int jmax=board[0].length;
+        for(int i=0;i<imax;i++){
+            s1=new HashSet<>();
+            for(int j=0;j<jmax;j++){
+                if(board[i][j]=='.')continue;
+                if(!s1.add(board[i][j]))return false;
             }
         }
-        HashMap<Character,Integer> map2=new HashMap<>();
-        for(int i=0;i<board.length;i++){
-            map2=new HashMap<>();
-            for(int j=0;j<board[0].length;j++){
-                char a=board[j][i];
-                if(a=='.')continue;
-                map2.put(a,map2.getOrDefault(a,0)+1);
-                if(map2.get(a)>1)return false;
+        for(int i=0;i<imax;i++){
+            s1=new HashSet<>();
+            for(int j=0;j<jmax;j++){
+                if(board[j][i]=='.')continue;
+                if(!s1.add(board[j][i]))return false;
             }
         }
-        
-        HashMap<Character,Integer> map3=new HashMap<>();
-        map2=new HashMap<>();
-        map1=new HashMap<>();
+        s1=new HashSet<>();
 
-        for(int i=0;i<board.length;i++){
+        for(int i=0;i<imax;i++){
             if(i==3){
-                map1=new HashMap<>();
-                map2=new HashMap<>();
-                map3=new HashMap<>();
-
+                s1=new HashSet<>();
+                s2=new HashSet<>();
+                s3=new HashSet<>();
             }
             if(i==6){
-                map1=new HashMap<>();
-                map2=new HashMap<>();
-                map3=new HashMap<>();
-
+                s1=new HashSet<>();
+                s2=new HashSet<>();
+                s3=new HashSet<>();
             }
-            for(int j=0;j<board[0].length;j++){
-                
-                char a=board[i][j];
-                if(a=='.')continue;
-                if(j>-1 && j<3){
-                    map1.put(a,map1.getOrDefault(a,0)+1);
-                    if(map1.get(a)>1)return false;
-
-                }
+            for(int j=0;j<jmax;j++){
                 if(j>2 && j<6){
-                    map2.put(a,map2.getOrDefault(a,0)+1);
-                    if(map2.get(a)>1)return false;
-
+                    if(board[i][j]=='.')continue;
+                    if(!s2.add(board[i][j]))return false;
                 }
-                if(j>5 && j<9){
-                    map3.put(a,map3.getOrDefault(a,0)+1);
-                    if(map3.get(a)>1)return false;
-
+                else if(j>5){
+                    if(board[i][j]=='.')continue;
+                    if(!s3.add(board[i][j]))return false;
+                }
+                else{
+                    if(board[i][j]=='.')continue;
+                    if(!s1.add(board[i][j]))return false;
                 }
                 
 
             }
         }
         return true;
-
         
-
     }
 }
