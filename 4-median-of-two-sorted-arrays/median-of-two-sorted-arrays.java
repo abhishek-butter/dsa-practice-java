f2f8a63902a962d3b22129e4daf3696c4e0721b5
Abhishek <abhishekvaiybhav@gmail.com>
2026-07-29 23:25:52 +0530

    Time: 1 ms (100.00%) | Memory: 48.7 MB (72.19%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -3,40 +3,32 @@ class Solution {
         if(nums1.length>nums2.length){
             return findMedianSortedArrays(nums2,nums1);
         }
-        
+        int al=0,ar=0,bl=0,br=0;
 
-        int left=0;
-        int right=(nums1.length<nums2.length)?nums1.length:nums2.length;
-        int aleft=0;
-        int aright=0;
-        int bleft=0;
-        int bright=0;
-        int[] small=nums1;
-        int[] big=nums2;
-        
-        
-        
-        while(left<=right){
-            int leftp=left+(right-left)/2;
-            int rightp=((small.length+big.length+1)/2)-leftp;
-            aleft=(leftp==0)?Integer.MIN_VALUE:small[leftp-1];
-            aright=(leftp==small.length)?Integer.MAX_VALUE:small[leftp];
+        int l=0;
+        int r=nums1.length;
+        while(l<=r){
+            int pa=l+(r-l)/2;
+            int pb=(nums1.length+nums2.length+1)/2-pa;
+            al=(pa!=0)?nums1[pa-1]:Integer.MIN_VALUE;
+            ar=(pa!=nums1.length)?nums1[pa]:Integer.MAX_VALUE;
+            bl=(pb!=0)?nums2[pb-1]:Integer.MIN_VALUE;
+            br=(pb!=nums2.length)?nums2[pb]:Integer.MAX_VALUE;
 
-            bright=(rightp!=big.length)?big[rightp]:Integer.MAX_VALUE;
-            bleft=(rightp!=0)?big[rightp-1]:Integer.MIN_VALUE;
-
-            if(bleft<=aright && aleft<=bright){
+            if(al<=br && bl<=ar){
                 if((nums1.length+nums2.length)%2==0){
-                    return ((((double)Math.max(aleft,bleft))+Math.min(aright,bright))/2.0);
+                    return ((double)Math.max(al,bl)+(double)Math.min(ar,br))/2.0;
+
                 }
-                else return Math.max(aleft,bleft);
+                else return (double)Math.max(al,bl);
             }
-            else if ( bleft>aright)left=leftp+1;
-            else if (aleft>bright)right=leftp-1;
-
-
+            else if(al>br)r=pa-1;
+            else if(bl>ar)l=pa+1;
+            
+            
         }
         return 0;
+
         
     }
 }
