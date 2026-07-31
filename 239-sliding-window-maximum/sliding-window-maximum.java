a4f6f7121427fe44557847a295756cf1bca3d01c
Abhishek <abhishekvaiybhav@gmail.com>
2026-07-31 08:55:08 +0530

    Time: 27 ms (94.54%) | Memory: 143.2 MB (94.39%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -4,14 +4,14 @@ class Solution {
         int c=0;
         int left=0;
         
-        Deque<Pair<Integer,Integer>> dq=new ArrayDeque<>();
+        Deque<Integer> dq=new ArrayDeque<>();
         for(int i=0;i<nums.length;i++){
-            while(!dq.isEmpty() && nums[i]>=dq.peekLast().getKey())dq.pollLast();
-            dq.offerLast(new Pair<>(nums[i],i));
+            while(!dq.isEmpty() && nums[i]>=nums[dq.peekLast()])dq.pollLast();
+            dq.offerLast(i);
 
             if(i-left+1==k){
-                while(!dq.isEmpty() && dq.peekFirst().getValue()<left)dq.pollFirst();
-                maxarray[c++]=dq.peek().getKey();
+                while(!dq.isEmpty() && dq.peekFirst()<left)dq.pollFirst();
+                maxarray[c++]=nums[dq.peekFirst()];
                 
                 left++;
 
