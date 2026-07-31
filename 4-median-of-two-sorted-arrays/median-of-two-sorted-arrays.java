8569988d407a6eb311ee98c27f4b5c34bedbbbc1
Abhishek <abhishekvaiybhav@gmail.com>
2026-07-31 09:08:50 +0530

    Time: 1 ms (100.00%) | Memory: 49 MB (16.07%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,41 +1,38 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if(nums1.length>nums2.length) return findMedianSortedArrays(nums2,nums1);
-
+        if(nums1.length>nums2.length)return findMedianSortedArrays(nums2,nums1);
         int left=0;
         int right=nums1.length;
-        int al,ar,bl,br;
-        int cons=(nums1.length+nums2.length+1)/2;
+        int al,bl,ar,br;
+        int tl=nums1.length+nums2.length;
+
         while(left<=right){
+
             int pa=(left+(right-left)/2);
-            int pb=cons-pa;
+            int pb=((tl+1)/2)-pa;
 
             al=(pa!=0)?nums1[pa-1]:Integer.MIN_VALUE;
-            ar=(pa!=nums1.length)?nums1[pa]:Integer.MAX_VALUE;
-
             bl=(pb!=0)?nums2[pb-1]:Integer.MIN_VALUE;
+
+            ar=(pa!=nums1.length)?nums1[pa]:Integer.MAX_VALUE;
             br=(pb!=nums2.length)?nums2[pb]:Integer.MAX_VALUE;
 
             if(al<=br && bl<=ar){
-                if((nums1.length+nums2.length)%2==0){
-                    return (Math.max(al,bl)+(double)Math.min(ar,br))/2.0;
+                if(tl%2==0){
+                    return ((double)Math.max(bl,al)+Math.min(br,ar))/2.0;
 
                 }
-                else {
-                    return Math.max(al,bl);
-
-                }
-
+                else return (double)Math.max(bl,al);
+                
             }
-            else if(al>br){
-                right=pa-1;
+            else if(al>br)right=pa-1;
+            else left=pa+1;
 
-            }
-            else if(bl>ar) left=pa+1;
 
 
         }
         return 0;
+
         
     }
 }
