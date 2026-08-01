0aa1ece43adbc21e7ce4d6a590328a77ceeb49e1
Abhishek <abhishekvaiybhav@gmail.com>
2026-08-01 12:20:41 +0530

    Time: 0 ms (100.00%) | Memory: 48 MB (57.79%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -6,25 +6,15 @@ class Solution {
             nn=Math.abs(nn);
             
         }
-
         long t=nn;
         int c=0;
-
         double r=1;
-        double cp=x;
-        
+        double cp=x;        
         while(t!=0){
-           
-            
-            if(t%2==1)r*=(cp);
-             cp*=cp;
-
+        if(t%2==1)r*=(cp);
+            cp*=cp;
             t=t/2;
-
         }
-
-        
-   
         return r;
 
 
