e1c554f95e76954526a415086062adf3d745a76e
Abhishek <abhishekvaiybhav@gmail.com>
2026-08-01 13:46:05 +0530

    Time: 36 ms (30.64%) | Memory: 59.2 MB (46.00%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,30 +1,39 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> li=new ArrayList<>();
+        List<List<Integer>> l=new ArrayList<>();
         Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++){
-            int l=i+1;
-            int r=nums.length-1;
-            if(i>0 && nums[i]==nums[i-1])continue;
-            while(l<r){
-               
+        for(int i=0;i<nums.length-2;i++){
+            int left=i+1;
+            int right=nums.length-1;
+            while(left<right){
+                int sum=nums[i]+nums[left]+nums[right];
                 
-                
-                int s=nums[i]+nums[l]+nums[r];
-                if(s>0)r--;
-                else if(s<0)l++;
-                else {
-                    li.add(Arrays.asList(nums[i],nums[l],nums[r]));
-                    l++;
-                    r--;
-                    while(l<nums.length-1 && l>0 && nums[l]==nums[l-1])l++;
-                    while(r>0 && r<nums.length-1 && nums[r]==nums[r+1])r--;
-                    
+                if(sum==0){
+                    l.add(List.of(nums[i],nums[left],nums[right]));
+                    left++;
+                    right--;
+                    while(left!=nums.length-1 && nums[left-1]==nums[left]){
+                        left++;
+                    }
+                    while(right!=0 && nums[right+1]==nums[right]){
+                        right--;
+                    }
+    
                 }
+                else if(sum>0){
+                    right--;
+                    continue;
+                }
+                else {
+                    left++;
+                    continue;
+                }
+                while(i!=nums.length-2 && nums[i+1]==nums[i])i++;
                 
             }
+           
         }
-        return li;
+        return l;
         
     }
 }
