8762c94a47a63e424eab2a1598c6d64e3982f95c
Abhishek <abhishekvaiybhav@gmail.com>
2026-08-08 23:16:35 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (28.13%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -6,16 +6,16 @@ class Solution {
             int mid=left+(right-left)/2;
             if(nums[mid]==target)return mid;
             else if(nums[left]<=nums[mid]){
-                if(target<nums[mid] && target>=nums[left])right=mid-1;
+                if(target>=nums[left] && target<nums[mid])right=mid-1;
                 else left=mid+1;
             }
-            else{
-                if(target>nums[mid] && target<=nums[right])left=mid+1;
+            else {
+                if(target<=nums[right] && target>nums[mid])left=mid+1;
                 else right=mid-1;
-
             }
         }
         return -1;
+
         
     }
 }
